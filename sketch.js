89ac1ffde49b4b09ee0b34319671701cea6c3520
EzEvilGod <marco.ferri2005@gmail.com>
2022-11-11 21:27:47 +0100

    assegnazione variabili e creazione del canvas

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let x
+let y 
+let d
+let Vx
+let Vy
+
+function setup() 
+{
+    x = random(400)
+    y = random(400)
+    d = random(25,64)
+    Vx = - 10
+    Vy = 20
+    createCanvas(400, 400);
+}
+
